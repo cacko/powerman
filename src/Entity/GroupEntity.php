48f5bf25f8d72c865fa96ee2a94b463c787cb73d
cacko <alex@cacko.net>
2023-06-20 09:20:09 +0100

    Apparently works-for-me is a crappy excuse.

--- src/Entity/GroupEntity.php
+++ src/Entity/GroupEntity.php
@@ -4,19 +4,21 @@ namespace App\Entity;
 
 use DateTime;
 use DateTimeZone;
-use phpDocumentor\Reflection\Types\Static_;
+use Symfony\Component\Validator\Constraints\Date;
 
 class GroupEntity extends AbstractAppEntity
 {
     const TZ = 'Europe/London';
+
     public function __construct(
         protected string $id,
         protected string $label,
         protected string $suspendAt,
         protected string $resumeAt,
-        protected int $sleepInterval,
-        protected array $hostnames
-    ) {
+        protected int    $sleepInterval,
+        protected array  $hostnames
+    )
+    {
 
     }
 
@@ -25,6 +27,21 @@ class GroupEntity extends AbstractAppEntity
         $tz = new DateTimeZone(static::TZ);
         $now = new DateTime('now', $tz);
         $resumeAt = new DateTime("today {$this->resumeAt}", $tz);
+        $suspendAt = new DateTime("today {$this->suspendAt}", $tz);
+        if ($now > $suspendAt) {
+            return new DateTime("tomorrow {$this->resumeAt}", $tz);
+        }
+        if ($resumeAt > $now) {
+            return $resumeAt;
+        }
+        return null;
+    }
+
+    public function getNextTrigger(int $trigger): ?DateTime
+    {
+        $tz = new DateTimeZone(static::TZ);
+        $now = new DateTime('now', $tz);
+        $resumeAt = $trigger ? new DateTime("@{$trigger}", $tz) : new DateTime("today {$this->resumeAt}", $tz);
         $suspendAt = new DateTime("today {$this->suspendAt}", $tz);
         if ($now > $suspendAt) {
             return new DateTime("tomorrow {$this->resumeAt}", $tz);
--- src/Entity/WorkstationEntity.php
+++ src/Entity/WorkstationEntity.php
@@ -10,36 +10,49 @@ class WorkstationEntity extends AbstractAppEntity
     protected ?StateEntity $state = null;
 
     public function __construct(
-        protected string $hostname,
+        protected string      $hostname,
         protected GroupEntity $group,
-        protected string $location
-    ) {
+        protected string      $location
+    )
+    {
 
     }
 
     protected function loadState()
     {
-        if(!file_exists($this->location)) {
+        if (!file_exists($this->location)) {
             $default = $this->group->getDefaultTrigger()?->getTimestamp() ?: 0;
-            $this->writeState([
+            $config = [
                 'trigger' => $default,
+                'ack' => 0
+            ];
+            return $this->writeState($config);
+        }
+        $config = Yaml::parseFile($this->location);
+
+        $state = new StateEntity(...$config);
+        $trigger = $state['trigger'];
+        $nextTrigger = $this->group->getNextTrigger($trigger)?->getTimestamp() ?: 0;
+        if ($trigger != $nextTrigger) {
+            return $this->writeState([
+                'trigger' => $nextTrigger,
                 'ack' => 0
             ]);
         }
-        $data = Yaml::parseFile($this->location);
-        return new StateEntity(...$data);
+        return $state;
     }
 
-    protected function writeState(array $config)
+    protected function writeState(array $config): StateEntity
     {
         $yaml = Yaml::dump($config);
         file_put_contents($this->location, $yaml);
+        return new StateEntity(...$config);
     }
 
 
     public function offsetGet(mixed $offset): mixed
     {
-        return match($offset) {
+        return match ($offset) {
             'state' => $this->loadState(),
             default => parent::offsetGet($offset),
         };
