113135f36ce641e4019250a45e9798a8955cf102
cacko <alex@cacko.net>
2023-06-20 04:29:48 +0100

    Give me a break, it's 2am.  But it works now.

--- assets/home.js
+++ assets/home.js
@@ -2,7 +2,15 @@ import "./styles/pages/home.sass";
 import typ_e_writer from "./js/typewriter.js";
 import cl_ock from "./js/clock.js";
 import {Rotator} from "./js/rotator.js";
+import DateTimePicker from "material-datetime-picker";
 
+const picker = new DateTimePicker()
+    .on('submit', (val) => console.log(`data: ${val}`))
+    .on('open', () => console.log('opened'))
+    .on('close', () => console.log('closed'));
+
+document.querySelector('.c-datepicker-btn')
+    .addEventListener('click', () => picker.open());
 
 document.querySelectorAll(".typewriter").forEach((el) => {
     typ_e_writer.write(el);
