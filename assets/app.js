77f16f283a7dba494e20b6259d007189fb0902d0
cacko <alex@cacko.net>
2023-06-21 07:23:23 +0100

    A long time ago, in a galaxy far far away...

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,13 @@
 import './styles/app.sass';
+import 'animate.css';
+import {Rotator} from "./js/rotator.js";
+import {Animator} from "./js/animator";
+
+window.addEventListener("load", () => {
+    Rotator.start();
+    Animator.start();
+});
+window.addEventListener("beforeunload", () => {
+    Rotator.stop()
+    Animator.stop();
+});
--- assets/home.js
+++ assets/home.js
@@ -1,7 +1,6 @@
 import "./styles/pages/home.sass";
 import typ_e_writer from "./js/typewriter.js";
 import cl_ock from "./js/clock.js";
-import {Rotator} from "./js/rotator.js";
 import DateTimePicker from "material-datetime-picker";
 
 const picker = new DateTimePicker()
@@ -17,11 +16,4 @@ document.querySelectorAll(".typewriter").forEach((el) => {
 
 document.querySelectorAll(".clock").forEach((el) => {
     cl_ock.start(el);
-});
-
-window.addEventListener("load", () => {
-    Rotator.start();
-});
-window.addEventListener("beforeunload", () => {
-    Rotator.stop()
 });
