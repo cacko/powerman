44a7e2c2e2a6145612766c4fa69d54906e97063c
cacko <alex@cacko.net>
2023-08-07 04:52:56 +0100

    At times like this I wish I was a Garbage Man.

--- assets/home.js
+++ assets/home.js
@@ -1,9 +1,12 @@
 import "./styles/pages/home.scss";
+import "./styles/components/_toastify.scss";
 import cl_ock from "./js/clock.js";
 import {DTSelector} from "./js/dt_selector.js";
+import {Messages} from "./js/messages";
 
 document.querySelectorAll(".clock").forEach((el) => {
     cl_ock.start(el);
 });
 
 DTSelector.start();
+Messages.start();
--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,22 +1,29 @@
-// import random from "lodash-es/random";class _messages {
-//
-//     constructor(nodes) {
-//         this.shown = false;
-//         this.nodes = nodes;
-//         this.toasts = [...nodes].map(toastEl => new Toast(toastEl));
-//     }
-//
-//     start() {
-//         if (!this.shown) {
-//             this.toasts.forEach(t => t.show());
-//             this.shown = true;
-//         }
-//         return true;
-//     }
-//
-//     stop() {
-//         return true;
-//     }
-// }
-//
-// export const Messages = new _messages(document.querySelectorAll(".toast"));
+import Toastify from 'toastify-js';
+
+class _messages {
+
+    constructor(nodes) {
+        this.shown = false;
+        this.toasts = [...nodes].map(toastEl => new Toastify({
+            text: toastEl.querySelector(".body").innerText.trim(),
+            gravity: "bottom", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            duration: 10000
+        }));
+    }
+
+    start() {
+        if (!this.shown) {
+            this.toasts.forEach(t => t.showToast());
+            this.shown = true;
+        }
+        return true;
+    }
+
+    stop() {
+        return true;
+    }
+}
+
+export const Messages = new _messages(document.querySelectorAll(".toast"));
